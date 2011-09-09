fe9fd39729c098f6f279d716bfc02a704e826f8c
Gboivin <boivin.gregoire@gmail.com>
2011-09-09 15:05:11 -0400

    Add jquery validate.

--- form.php
+++ form.php
@@ -1,18 +1,25 @@
 <html>
   <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script type="text/javascript" src="http://dev.jquery.com/view/trunk/plugins/validate/jquery.validate.js"></script>
   </head>
   <body>
+    <script>
+      $(document).ready(function(){
+        $("#inscriptionForm").validate();
+      });
+      </script>
     Formulaire d'inscription
-    <form action='success.php' method='post'>
+    <form id='inscriptionForm' action='success.php' method='post'>
       <label for="fname">Prénom</label>
-    <input type='text' name='fname'/>
+    <input type='text' id='fname' name='fname' class='required'/>
     <label for="lname">Nom</label>
-    <input type='text' name='lname'>
+    <input type='text' id='lname' name='lname' class='required'>
     <label for="email">Email</label>
-    <input type='text' name='email'>
+    <input type='text' id='email' name='email' class='required email'>
     <label for="jour">Jour</label>
-    <select name='jour'>
+    <select name='jour' id='jour' class='required'>
       <?php for ($i = 1; $i < 32; $i++):?>
       <option value='<?php echo $i;?>'>
         <?php echo $i; ?>
@@ -20,7 +27,7 @@
       <?php endfor;?>
     </select>
     <label for="mois">Mois</label>
-    <select name='mois'>
+    <select id='mois' name='mois'>
       <?php for ($i = 1; $i < 12; $i++):?>
       <option value='<?php echo $i;?>'>
         <?php echo $i; ?>
@@ -28,7 +35,7 @@
       <?php endfor;?>
     </select>
     <label for="annee">Année</label>
-    <select name='annee'>
+    <select id='annee' name='annee'>
       <?php for ($i = 1900; $i < 2011; $i++):?>
       <option value='<?php echo $i;?>'>
         <?php echo $i; ?>
@@ -36,14 +43,14 @@
       <?php endfor;?>
     </select>
     <label for="sexe">Homme</label>
-    <input type='radio' name='sexe' value='h'/>
+    <input type='radio' id='sexeh' name='sexe' value='h' class='required'/>
     <label for="sexe">Femme</label>
-    <input type='radio' name='sexe' value='f'>
+    <input type='radio' id='sexef' name='sexe' value='f' class='required'>
     <label for="login">Login</label>
-    <input type='texte' name='login'/>
+    <input type='texte' id='login' name='login'/>
     <label for="pwd">Mot de passe</label>
-    <input type='password' name='pwd'/>
-    <input type='checkbox' name='condition'/> J'accepte les conditions de ce site
+    <input type='password' id='pwd'name='pwd' class='required'/>
+    <input type='checkbox' id='condition' name='condition' class='required check'> J'accepte les conditions de ce site
     <input type='submit'/>
     </form>
   </body>
