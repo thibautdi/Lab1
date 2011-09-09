02c3e82187c50091050e39a080761988202f198b
Olivier Wojdyniak <owojdyniak@gmail.com>
2011-09-09 15:34:31 -0400

    Ajouté cadre menu gauche

--- form.php
+++ form.php
@@ -3,8 +3,47 @@
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <script src="http://code.jquery.com/jquery-latest.js"></script>
     <script type="text/javascript" src="http://dev.jquery.com/view/trunk/plugins/validate/jquery.validate.js"></script>
+    <link rel="stylesheet" media="screen" type="text/css" title="Design" href="style.css" />
   </head>
   <body>
+  <div class="cadredumenugauche">
+    <div class="menu">
+	<ul  name="coordonnees" id="coordonnees">
+		<li id="Li1"  class="menu">Collaborateur<br/>
+		<span  class="collaborateur" id="collaborateur"></span><br/>
+		</li>
+
+		<li class="menu">Date<br/>
+		
+		<select   id="date" tabindex="10">
+           
+       	   </select><br/>
+		</li>
+	
+
+
+		<li class="menu">Urgence<br />
+		<select  name="urgence" id="urgence" tabindex="30">
+          </select><br/>
+		</li>
+
+		<li class="menu">Durée<br />
+		<select  name="duree" id="duree" tabindex="40">
+           
+       	   </select><br/>
+		</li>
+        <li class="menu">Environnement<br />
+		<select class="listbox1"  name="environnement" id="environnement" 
+                tabindex="45" size="5">
+           
+       	   </select>
+		</li>
+
+	</ul>
+	
+	</div>
+
+   </div>
     <script>
       $(document).ready(function(){
         $("#inscriptionForm").validate();
