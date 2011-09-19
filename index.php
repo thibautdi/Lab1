6c9a52568d7b445465be756b11d6c09d5c0e2ab9
Gboivin <boivin.gregoire@gmail.com>
2011-09-19 14:23:27 -0400

    Remplace menu codé par menu.php

--- index.php
+++ index.php
@@ -1,60 +1,8 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
-   <head>
-       <title>Accueil</title>
-       <?php include("header.php") ?>
-   </head>
-   <body>
-	
-	<div id="header">
-	</div>
-
-	<div id="connection">
-	  <p> Login : </p>
-	  <p> Password : </p>
-	</div>
-
-	<div id="contenu">
+<?php include 'header.php'; ?>
+  <div id="contenu">
 	  <p> Texte d'introduction, balise div de contenu</p>
 	</div>
-
-	<div id="menu">
-      <li><a href="">Accueil</a></li>
-      <li><a href="">Catégorie 1</a>
-        <ul>
-          <li><a href="">Catégorie 1.1</a>
-            <ul>
-              <li><a href="">Catégorie 1.1.1</a></li>
-              <li><a href="">Catégorie 1.1.2</a></li>
-              <li><a href="">Catégorie 1.1.3</a></li>
-            </ul>
-          </li>
-          <li><a href="">Catégorie 1.2</a>
-            <ul>
-              <li><a href="">Catégorie 1.2.1</a></li>
-              <li><a href="">Catégorie 1.2.2</a></li>
-              <li><a href="">Catégorie 1.2.3</a></li>
-            </ul>
-          </li>
-          <li><a href="">Catégorie 1.3</a></li>
-        </ul>
-      </li>
-      <li><a href="">Catégorie 2</a>
-        <ul>
-          <li><a href="">Catégorie 2.1</a></li>
-          <li><a href="">Catégorie 2.2</a></li>
-        </ul>
-      </li>
-      <li><a href="">Catégorie 3</a>
-        <ul>
-          <li><a href="">Catégorie 3.1</a></li>
-          <li><a href="">Catégorie 3.2</a></li>
-        </ul>
-      </li>
-      <li><a href="">Catégorie 4</a></li>
-   </div>
-
-	
+	<?php include 'menu.php'; ?>
 	<div id="pub">
 	  <script type="text/javascript"><!--
     google_ad_client = "ca-pub-7836030959373569";
