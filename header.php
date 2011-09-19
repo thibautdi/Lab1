d95fd4aa504c377659e9c465fb3bb8a825a073d4
Gboivin <boivin.gregoire@gmail.com>
2011-09-19 14:22:20 -0400

    Ajouts dans header.

--- header.php
+++ header.php
@@ -1,5 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+   <head>
+       <title>Accueil</title>
+       <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+       <link rel="stylesheet" type="text/css" href="style.css" />
+       <script type="text/javascript" src="scripts/jquery.validate.min.js"></script>
+   </head>
+   <body>
+	
+	<div id="header">
+	</div>
 
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="style.css" />
-
-
+	<div id="connection">
+	  <p> Login : </p>
+	  <p> Password : </p>
+	</div>
