4caa9fdf6a8fa537f013315d39d0eac4faf42262
Gboivin <boivin.gregoire@gmail.com>
2011-10-18 12:10:11 -0400

    Modifie css et ajoute page d'accueil.

---
+++ welcome.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <link rel="stylesheet" type="text/css" href="style/welcome.css" />
+    </head>
+    <body>
+      <a href='index.php'>
+        <div id='femme'>
+          <p>Femme</p>
+        </div>
+      </a>
+      <a href='index.php'>
+        <div id='homme'>
+          <p>Homme</p>
+        </div>
+      </a>
+    </body>
+  </html>
+
