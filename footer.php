01b7fd1a67ade75e55f611c1af45af0e2613548e
Gboivin <boivin.gregoire@gmail.com>
2011-11-13 03:08:36 -0500

    Retire commentaires html.

--- footer.php
+++ footer.php
@@ -1,5 +1,3 @@
-<!-- /*fichier de footer, comportant les crédits et informations*/ -->
-
 </div>
 <div id='footer'>
   <p>Site créé par Grégoire Boivin et Thibaut Diehl, Étudiants à l'École Polytechnique de Montréal. Tous droits réservés</p>
