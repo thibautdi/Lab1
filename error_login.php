f488079c6ac1904313e2d743874a9ad77aa5a189
Thibaut <thibaut.diehl@gmail.com>
2011-11-20 18:46:09 -0500

    Errors and successes pages.

---
+++ error_login.php
@@ -0,0 +1,7 @@
+<?php include 'header.php';?>
+<?php include 'menu.php';?>
+<div id='contenu'>
+  <p>Nom d'utilisateur ou mot de passe invalide, merci de réessayer.</p>
+</div>
+<?php include 'sidebar.php';?>
+<?php include 'footer.php';?>
---
+++ success_delete_club.php
@@ -0,0 +1,7 @@
+<?php include 'header.php';?>
+<?php include 'menu.php';?>
+<div id='contenu'>
+  <p>Le club a été correctement supprimé</p>
+</div>
+<?php include 'sidebar.php';?>
+<?php include 'footer.php';?>
