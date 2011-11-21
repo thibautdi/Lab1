d31a9b192b1b1e55ebecf83117f3ffd6afd51bdc
Gboivin <boivin.gregoire@gmail.com>
2011-11-20 22:53:35 -0500

    Changement position top 5.

--- location.php
+++ location.php
@@ -9,13 +9,14 @@ include 'menu.php';
   <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
   <article>
     <p>Géolocalisation en cours: <span id="status">...</span></p>
+      <div id='top_0' class='closest'></div>
+      <div id='top_1' class='closest'></div>
+      <div id='top_2' class='closest'></div>
+      <div id='top_3' class='closest'></div>
+      <div id='top_4' class='closest'></div>
   </article>
   <p id='position'><p>
-  <div id='top_0' class='closest'></div>
-  <div id='top_1' class='closest'></div>
-  <div id='top_2' class='closest'></div>
-  <div id='top_3' class='closest'></div>
-  <div id='top_4' class='closest'></div>    
+  
 </div>
 <?php
 include 'sidebar.php';
